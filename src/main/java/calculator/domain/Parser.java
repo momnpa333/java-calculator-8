de221935e4ae7f49313912430f9b2981c80b40c0
momnpa333 <momnpa333@gmail.com>
2025-10-16 15:08:55 +0900

    feat: 파서 구분자 구분 및 피연산자 제공 기능 구현

--- src/main/java/calculator/domain/Parser.java
+++ src/main/java/calculator/domain/Parser.java
@@ -1,9 +1,43 @@
 package calculator.domain;
 
+import java.util.regex.Pattern;
+
 public class Parser {
-    //TODO 입력 문자열 파싱
+    private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final String CUSTOM_DELIMITER_PREFIX = "//";
+    private static final String CUSTOM_DELIMITER_SUFFIX = "\n";
 
-    //TODO 기본 구분자 처리
+    private String getCustomDelimiter(String input) {
+        int startIndex = CUSTOM_DELIMITER_PREFIX.length();
+        int endIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX);
+        return input.substring(startIndex, endIndex);
+    }
 
-    //TODO 커스텀 구분자 처리
+    // 구분자 정규식(기본 + 커스텀) 반환
+    private String getDelimiterRegex(String input) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            String custom = getCustomDelimiter(input);
+            return DEFAULT_DELIMITER_REGEX + "|" + Pattern.quote(custom);
+        }
+        return DEFAULT_DELIMITER_REGEX;
+    }
+
+    private String removeCustomDelimiter(String input) {
+        if (input.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int endIndex = input.indexOf(CUSTOM_DELIMITER_SUFFIX) + CUSTOM_DELIMITER_SUFFIX.length();
+            return input.substring(endIndex);
+        }
+        return input;
+    }
+
+    // 피연산자 구하기
+    public String[] getOperand(String input) {
+        if (input == null || input.isBlank()) {
+            return new String[0];
+        }
+        String delimiterRegex = getDelimiterRegex(input);
+        String numbersPart = removeCustomDelimiter(input);
+        return numbersPart.split(delimiterRegex);
+    }
+
 }
