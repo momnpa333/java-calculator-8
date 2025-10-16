22cb50b24f4df64dd2920c9ebcaf2a301aa2df62
momnpa333 <momnpa333@gmail.com>
2025-10-16 15:21:36 +0900

    feat: Application 의존성 주입 및 실행

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,9 +1,20 @@
 package calculator;
 
+import calculator.domain.Calculator;
+import calculator.domain.Parser;
+import calculator.view.InputView;
+import calculator.view.OutputView;
+import calculator.view.Viewer;
+
 public class Application {
 
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Viewer viewer = new Viewer(outputView, inputView);
+        Parser parser = new Parser();
+        Calculator calculator = new Calculator(parser, viewer);
+        calculator.run();
     }
 }
