7220816c8e2ce033e1505db7b822849340961da7
momnpa333 <momnpa333@gmail.com>
2025-10-19 22:59:55 +0900

    refact(OutputView): RESULT_MESSAGE static final로 변경

--- src/main/java/calculator/view/OutputView.java
+++ src/main/java/calculator/view/OutputView.java
@@ -1,8 +1,9 @@
 package calculator.view;
 
 public class OutputView {
+    static final String RESULT_MESSAGE = "결과 : ";
 
     public void printResult(String result) {
-        System.out.println("결과: " + result);
+        System.out.println(RESULT_MESSAGE + result);
     }
 }
